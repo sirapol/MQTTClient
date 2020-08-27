947975d9f9265b7d4c1fc0cf5b971e585ebeda28
sirapol <s.sirapol@hotmail.com>
2020-08-27 11:32:59 +0700

    commend file

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ MQTTClient
 python version : 3.7.3
 
 Hardware : Raspberry-Pi4
+
+Require Library
+- https://pypi.org/project/paho-mqtt/
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #Python version : 3.7.3
 
 import paho.mqtt.client as mqtt
+
 client = mqtt.Client()
-host="58.64.25.160"
-port=500
-mqUsr="sirapsri"
-mqPas="sirapsri"
-client.username_pw_set(mqUsr,mqPas)
-client.connect(host,port)
-client.publish("TEST/MQ","Hello")
-client.disconnect()
+
+host="yourHost"
+port=500        # your port
+mqUsr="user"
+mqPas="password"
+
+client.username_pw_set(mqUsr,mqPas)     # If your broker is not require user,pass. Let comment this.
+client.connect(host,port)               # Open connection
+client.publish("TEST/MQ","Hello")       # "TEST/MQ" is Tag , "Hellow" is data
+client.disconnect()                     # Close connection
